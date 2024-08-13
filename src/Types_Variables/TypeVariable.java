fdc1991ceee886a2941fc1b0ae686b39161e2905
Eduardo Pacheco <jpachecom1703@alumno.ipn.mx>
2024-08-12 20:10:17 -0600

    Types and variables

---
+++ src/Types_Variables/TypeVariable.java
@@ -0,0 +1,24 @@
+package Types_Variables;
+
+public class TypeVariable {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world!!");
+		
+		int age = 21; //integer
+		double earnings = 2500.50; //numbers with decimals
+		long test = 1222222222L; //big numbers
+		short test2 = 13555;
+		byte test3 = 20; //8 bits
+		float test4 = 2.5f; 
+		
+		System.out.println("Age: " + age);
+		System.out.println("Earnings: " + earnings);
+		
+		int cast = (int) earnings; //doing a variable conversion
+		
+		System.out.println(cast);
+	}
+
+}
